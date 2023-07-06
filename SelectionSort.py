aa93e873f1151674f2c2c8eb60d59d21427e48de
seydaibro <138208371+seydaibro@users.noreply.github.com>
2023-07-05 23:59:24 -0700

    Create SelectionSort.py

---
+++ SelectionSort.py
@@ -0,0 +1,14 @@
+
+    def select(self, arr, i):
+        arr.sort()
+        return arr[i-1]
+
+    def selectionSort(self, arr, n):
+        for i in range(n):
+            min_idx = i
+            for j in range(i+1, n):
+                if arr[j] < arr[min_idx]:
+                    min_idx = j
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        return arr
+      
